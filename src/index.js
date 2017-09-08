a55755511b14a97b0ec50778e86408292fdf7e70
Julian Grinblat <julian@dotcore.co.il>
2017-09-08 23:30:03 +0900

    Change to better cordova event listener practices

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 let state = false;
 
-document.addEventListener('pause', () => {
-  state = true;
-}, false);
+// https://cordova.apache.org/docs/en/latest/cordova/events/events.html
+// Note: Applications typically should use document.addEventListener
+// to attach an event listener once the deviceready
+document.addEventListener('deviceready', () => {
+  document.addEventListener('pause', () => {
+    state = true;
+  }, false);
 
-document.addEventListener('resume', () => {
-  state = false;
-}, false);
+  document.addEventListener('resume', () => {
+    state = false;
+  }, false);
+});
 
 export default function isBackground() {
   return state;
